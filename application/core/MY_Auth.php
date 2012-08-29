0d91e4636bd2feffba8b733831919bbcf502cbac
Cedric ROBERT <c.robert@arwen-tech.fr>
2012-08-29 19:42:14 +0200

    préparation MY_Auth
    classe hérité pour la partie enregistrée

--- application/core/MY_Auth.php
+++ application/core/MY_Auth.php
@@ -1,13 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Admin extends CI_Controller {
+class MY_Auth extends CI_Controller {
 	
 	function __construct()
 	{
 		parent::__construct();
 		//On test si l'utilisateur est admin ou pas. 
+		//~ if (! isset ($this->session->userdata('logged'))
+		//~ {
+			//~ //S'il ne l'est pas on le redirige sur la page de log
+			//~ redirect('login', 'refresh');
+		//~ }
 		
-		//S'il ne l'est pas on le redirige sur la page de log
 	}
 
 	public function index()
