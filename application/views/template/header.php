718d343a09ff23a6b13cfb81a24962f645aba919
Cedric ROBERT <c.robert@arwen-tech.fr>
2012-08-29 19:31:54 +0200

    fix header et base_url

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 	<LINK rel="stylesheet" type="text/css" href="<?php echo site_url('assets/css/styles.css');?>">
-	<h2>Bienvenue sur le Logiciel de Trèz</h2>
+	<h2>Bienvenue sur le Logiciel de Tr&egrave;z</h2>
 	<?php if (isset ($title)):?>
 	<h3><?php echo $title;?></h3>
 	<?php endif;?>
