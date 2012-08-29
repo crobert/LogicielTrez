fcf0da3c616ae222dfbed8d0b8ae2de7b2058f23
Cedric ROBERT <c.robert@arwen-tech.fr>
2012-08-29 19:47:36 +0200

    simulation accueil+login

---
+++ application/controllers/accueil.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Accueil extends CI_Controller {
+
+
+	public function index()
+	{
+		$this->load->view('template/header');
+		$this->load->view('template/menu');
+		//~ $this->load->model('constantes', '', TRUE);
+		
+		//		return $query->result();
+		//$this->load->view('template/footer');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,12 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Login extends CI_Controller {
 
 
 	public function index()
 	{
-		$this->load->view('template/header');
-		$this->load->view('template/menu');
+		echo "<a href='".base_url()."accueil'>Pour simuler une connexion cliquer ce lien</a>"; 
 		//~ $this->load->model('constantes', '', TRUE);
 		
 		//		return $query->result();
