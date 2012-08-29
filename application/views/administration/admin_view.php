61246d4ae09d53ed331feb152acea34b642c521f
Cedric ROBERT <c.robert@arwen-tech.fr>
2012-08-29 19:29:12 +0200

    2nd commit

--- application/views/administration/admin_view.php
+++
@@ -1,4 +0,0 @@
-Voil&agrave; la page d'accueil d'administration, choisissez le module &agrave; &eacute;diter
-<br/><br/>
-<a href = "<?php echo site_url('administration/utilisateur'); ?>" > Module utilisateur </a> <br/>
-<a href = "<?php echo site_url('administration/listes'); ?>"> Listes</a> (en cours de dev)<br/>
--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -1,16 +1,4 @@
 <div id=menu>
 	<a href =<?php echo site_url();?> >Accueil</a><br/>
-	<a href =<?php echo site_url('admin/'); ?>>Admin</a><br/>
-	<a href =<?php echo site_url('administration/activite'); ?>>Administration/Activit&eacute;</a><br/>
-	<a href =<?php echo site_url('administration/agenda'); ?>>Administration/Agenda</a><br/>
-	<a href =<?php echo site_url('administration/commune'); ?>>Administration/Commune</a><br/>
-	<a href =<?php echo site_url('administration/constante'); ?>>Administration/Constante</a><br/>
-	<a href =<?php echo site_url('administration/contact'); ?>>Administration/Contact</a><br/>
-	<a href =<?php echo site_url('administration/contrat'); ?>>Administration/Contrat</a><br/>
-	<a href =<?php echo site_url('administration/intervention'); ?>>Administration/Intervention</a><br/>
-	<a href =<?php echo site_url('administration/site'); ?>>Administration/Site</a><br/>
-	<a href =<?php echo site_url('administration/tache'); ?>>Administration/T&acirc;che</a><br/>
-	<a href =<?php echo site_url('administration/tiers'); ?>>Administration/Tiers</a><br/>
-	<a href =<?php echo site_url('administration/utilisateur'); ?>>Administration/Utilisateur</a><br/>
-	<a href =<?php echo site_url('administration/listes'); ?>>Administration/Listes</a><br/>
+
 </div>
