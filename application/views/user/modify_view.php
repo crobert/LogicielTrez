0d9d8c1f7938e00a0543f8477aa588a26d894df1
Romaric Drigon <romaric.drigon@gmail.com>
2012-09-02 00:33:00 +0200

    Complété le contrôler utilisateurs

---
+++ application/views/user/modify_view.php
@@ -0,0 +1,35 @@
+<h1>Modifier l'utilisateur <?php echo $username; ?></h1>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('user/modify/'.$id, array('class' => 'form-horizontal')); ?>
+<div class="control-group">
+    <label class="control-label" for="inputUsername">Nom d'utilisateur</label>
+    <div class="controls">
+        <input type="text" id="inputUsername" placeholder="Nom d'utilisateur" name="username" value="<?php echo set_value('username', $username); ?>" required>
+    </div>
+</div>
+<div class="control-group">
+    <label class="control-label" for="inputPassword">Mot de passe</label>
+    <div class="controls">
+        <input type="password" id="inputPassword" placeholder="Mot de passe" name="password">
+    </div>
+</div>
+<div class="control-group">
+    <label class="control-label" for="inputPasswordConf">Confirmation du mot de passe</label>
+    <div class="controls">
+        <input type="password" id="inputPasswordConf" placeholder="Mot de passe" name="password_confirmation">
+    </div>
+</div>
+<div class="control-group">
+    <label class="control-label" for="inputType">Type</label>
+    <div class="controls">
+        <input type="text" id="inputType" placeholder="admin" name="type" value="<?php echo set_value('type', $type); ?>" required>
+    </div>
+</div>
+<div class="control-group">
+    <div class="controls">
+        <button type="submit" class="btn btn-primary">Modifier</button>
+    </div>
+</div>
+<?php echo form_close(); ?>
