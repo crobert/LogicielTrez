03211d128e64659cf4220d7ab47faa0620a4b81a
Romaric Drigon <romaric.drigon@gmail.com>
2012-09-02 00:36:49 +0200

    Ajouté un lien vers les exercices

--- application/views/_template/menu_view.php
+++ application/views/_template/menu_view.php
@@ -3,6 +3,7 @@
             <div class="container">
                 <a class="brand" href="<?php echo site_url();?>">Logiciel Trez</a>
                 <ul class="nav">
+                    <li><a href="<?php echo site_url('exercice/index');?>">Exercices</a></li>
                     <li><a href="<?php echo site_url('user/index');?>">Utilisateurs</a></li>
                 </ul>
             </div>
