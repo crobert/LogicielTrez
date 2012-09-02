8843ebfd66ff9d4283516303c0ba12ee38753ece
crobert <cr.cedric.robert@gmail.com>
2012-09-02 17:58:36 +0200

    gestions des categories de chaque budget

--- application/controllers/categorie.php
+++ application/controllers/categorie.php
@@ -2,25 +2,26 @@
 
 class Categorie extends MY_Auth
 {
-	public function index()
+	public function index($id_budget)
 	{
 		$this->load->model('categorie_model');
-		$data['categories'] = $this->categorie_model->list_categorie(); 
+		$data['categories'] = $this->categorie_model->get_categorie_by_budget($id_budget); 
 		
+		$data['id_budget'] = $id_budget;
         $data['_view'] = 'categorie/list_view';
         $this->load->view('default_template', $data);
 	}
 	
-	public function add()
+	public function add($id_budget)
 	{
 		//Règles pour tous les champs
 		$this->load->library('form_validation');
 		$this->form_validation->set_rules('nom', 'Nom', 'trim|required|xss_clean');
 		$this->form_validation->set_rules('description', 'Description', 'trim|required|xss_clean');
-		$this->form_validation->set_rules('id_budget', 'Budget', 'trim|required|xss_clean');
 
 		if($this->form_validation->run() == FALSE)
 		{
+			$data['id_budget'] = $id_budget;
 			$data['_view'] = 'categorie/nouveau_view';
 			$this->load->view('default_template', $data);
 		}
@@ -29,16 +30,16 @@ class Categorie extends MY_Auth
 			$data= array(
 					'nom' => $this->input->post('nom'), 
 					'description' => $this->input->post('description'), 
-					'id_budget' => $this->input->post('id_budget')
+					'id_budget' => $id_budget
 			);
 		$this->load->model('categorie_model');
 			$this->categorie_model->add_categorie($data); 
-			redirect('categorie', 'refresh');
+			redirect('categorie/index/'.$id_budget, 'refresh');
 		}
 		
 	}
 	
-	public function modify($id)
+	public function modify($id, $id_budget)
 	{
 		$this->load->model('categorie_model');
 		$categorie = $this->categorie_model->get_categorie($id); 
@@ -46,32 +47,32 @@ class Categorie extends MY_Auth
 		$this->load->library('form_validation');
 		$this->form_validation->set_rules('nom', 'Nom', 'trim|required|xss_clean');
 		$this->form_validation->set_rules('description', 'Description', 'trim|required|xss_clean');
-		$this->form_validation->set_rules('id_budget', 'Budget', 'trim|required|xss_clean');
 
 		if($this->form_validation->run() == FALSE)
 		{
-        $data['categorie'] = $categorie;
-        $data['_view'] = 'categorie/modifier_view';
-        $this->load->view('default_template', $data);
+			$data['id_budget'] = $id_budget;
+			$data['categorie'] = $categorie;
+			$data['_view'] = 'categorie/modifier_view';
+			$this->load->view('default_template', $data);
 		}
 		else
 		{
 			$data= array(
 					'nom' => $this->input->post('nom'), 
 					'description' => $this->input->post('description'), 
-					'id_budget' => $this->input->post('id_budget')
+					'id_budget' => $id_budget
 			);
 			
 			$this->categorie_model->modify_categorie($id, $data); 
-			redirect('categorie', 'refresh');
+			redirect('categorie/index/'.$id_budget, 'refresh');
 		}
 		
 	}
 	
-	public function delete($id)
+	public function delete($id, $id_budget)
 	{
 		$this->load->model('categorie_model');
 		$this->categorie_model->delete_categorie($id); 
-		redirect('categorie', 'refresh');
+		redirect('categorie/index/'.$id_budget, 'refresh');
 	}
 }
--- application/views/budget/list_view.php
+++ application/views/budget/list_view.php
@@ -13,7 +13,8 @@
         <tr>
             <td><?php echo $budget->id; ?></td>
             <td><?php echo $budget->nom; ?></td>
-            <td>
+            <td>			
+                <a href="<?php echo site_url().'categorie/index/'.$budget->id;?>">Accéder&nbsp;aux&nbsp;catégories</a>&nbsp;
                 <a href="<?php echo site_url().'budget/modify/'.$budget->id.'/'.$budget->id_exercice;?>">Modifier</a>&nbsp;
                 <a href="<?php echo site_url().'budget/delete/'.$budget->id.'/'.$budget->id_exercice;?>">Supprimer</a>
             </td>
