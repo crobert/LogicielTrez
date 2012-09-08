a66fba5b145c13ef8c6c99c2b8dfcdea2e4bab7d
Romaric Drigon <romaric.drigon@gmail.com>
2012-09-08 18:28:21 +0200

    Le menu n'apparaît plus pour les utilisateurs non connectés

--- application/views/_template/menu_view.php
+++ application/views/_template/menu_view.php
@@ -2,12 +2,14 @@
         <div class="navbar-inner">
             <div class="container">
                 <a class="brand" href="<?php echo site_url();?>">Logiciel Trez</a>
+                <?php if ($this->session->userdata('logged')) : ?>
                 <ul class="nav">
                     <li><a href="<?php echo site_url('exercice/index');?>">Exercices</a></li>
                     <li><a href="<?php echo site_url('tiers/index');?>">Tiers</a></li>
                     <li><a href="<?php echo site_url('user/index');?>">Utilisateurs</a></li>
                     <li><a href="<?php echo site_url('config/index');?>">Configuration</a></li>
                 </ul>
+                <?php endif; ?>
             </div>
         </div>
     </div>
